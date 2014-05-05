b0ce91e6cf3df7370b33db44a3c167c9755c74b0
Brad Green <bradgreen@infowest.com>
2014-05-05 15:26:50 -0600

    fixed syntax on jquery validation file

--- js/jquery_formvalidation.js
+++ js/jquery_formvalidation.js
@@ -10,29 +10,22 @@ $(document).ready(function()
 		rules:
 		{
 			firstname:
-			 {
+			{
 			 	required: true,
 			 	minlength: 2,
 			 	pattern: /^([A-Za-z])*(\s+[A-Za-z])?$/
-			  },
+			},
 			lastname:
-			  {
+			{
 			  	required: true,
 			  	pattern: /^([A-z\.\-])*$/
-			  },
-			 
-		},
-		
+			}
+		},	
 		//messages are what are displayed to the user 
 		messages:
 		{	
-		
 			firstname: "Please enter a valid first name",
-			lastname: "Please enter a valid last name",
-			
-		
-			}
-			
+			lastname: "Please enter a valid last name"
 		}
 	});
 });
