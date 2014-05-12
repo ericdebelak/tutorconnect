2502acfddac596fada0c5cb99e99b6e29552fa79
Eric Debelak <ericdebelak@gmail.com>
2014-05-12 14:03:44 -0600

    log scripts working

---
+++ php/log_proc.php
@@ -0,0 +1,48 @@
+<?php
+    // require the needed files
+    require_once("session.php");
+    require_once("/home/bradg/tutorconnect/config.php");
+    
+    try
+    {
+        
+        // grab the info from the form
+        $notes = $_POST["notes"];
+        $nextSteps = $_POST["nextSteps"];
+        $sessionId = $_POST["sessionId"];
+        $userId = $_POST["userId"];
+        $mysqli = Pointer::getMysqli();
+        
+        // build the session
+        $session = Session::getSessionBySessionId($mysqli, $sessionId);
+        
+        if($userId == $session->getTutorId())
+        {
+            $session->setTutorLog($notes);
+            $session->setTutorNextSteps($nextSteps);
+        }
+        
+        if($userId == $session->getStudentId())
+        {
+            $session->setStudentLog($notes);
+            $session->setStudentNextSteps($nextSteps);
+        }
+        
+        try
+        {
+            $session->update($mysqli);
+        }
+        catch(Exception $exception)
+        {
+            echo $exception;
+        }
+        var_dump($session);
+        var_dump($_POST);
+        echo "Success!";
+    }
+    catch(Exception $exception)
+    {
+        echo $exception;
+    }
+    
+?>
