2dee4f5d3f2e207213d6e8022121242e0d596e1a
Kirsten Eaton <kirstemmarie0@gmail.com>
2014-05-14 09:49:43 -0600

    updated login and cleaned out unnecessary files

--- php/form_Tutor.php
+++ php/form_Tutor.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
  <html>
 	<head>
-		<title>New Account</title>
+		<title>Login, login with Facebook log or login with Google</title>
 		<link rel="stylesheet" type="text/css" href="../css/stylesheetForm.css"/>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="http://malsup.github.com/jquery.form.js"></script>
 		<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
 		<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
 		<script src="../js/jquery_formvalidation.js" type="text/javascript"></script>
+	        <script src="facebook.js" type="text/javascript"></script>
+
 	</head>
 	<body>
 		<div class="page">
 		 
-			<h1>Create Account</h1>
-			<form method="post" id="signupForm" form actio="sendmail.php">
+		     <h1>Create Account</h1>
+		     <form id="userForm" method="post" action="facebook_user.php">
+  
+<!-- Help Eric I don't know what to do here -->
+		     <form method="post" id="signupForm" form action="sendmail.php">
 			         <fieldset>
 				   <p><label for="name">Name:</label><br/>
 				   <input type="text" size="25" id="name" name="name" /></p>
@@ -140,8 +145,43 @@
 					</div>
 					 
 					<input type="submit" value="Create Account" class="btn" />
-				 
-			</form>	
-		</div>
+									 
+		     </form>
+		     <form>
+		     <!-- facebook login and javascript begins here -->    
+		     </form><fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+				   </fb:login-button>
+				   <div id="status">
+				   </div>
+				   <div id="fb-root">
+				   </div>
+				   <div id="userStatus">
+				   </div>
+       + 			<form id="userForm" method="post" action="facebook_user.php">
+				       <input type="hidden" name="firstName" id="firstName" />
+				       <input type="hidden" name="lastName" id="lastName" />
+				       <input type="hidden" name="email" id="email" />
+		     </form>
+		     <form>
+		     <span id="signinButton">
+			 <span
+			   class="g-signin"
+			   data-callback="signinCallback"
+			   data-clientid="599980833060-1lgsbv7cpvaldfrp8roq2k0jf4m2ku0u.apps.googleusercontent.com"
+			   data-cookiepolicy="single_host_origin"
+			   data-requestvisibleactions="http://schemas.google.com/AddActivity"
+			   data-scope="https://www.googleapis.com/auth/plus.login">
+			 </span>
+		     </script>
+		     <script type="text/javascript">
+		     (function()
+		      {
+		      var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+		      po.src = 'https://apis.google.com/js/client:plusone.js';
+		      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		    })();
+		     </script>
+	      </div>
+		
 	</body>
 </html>
