6394ef01cd092c4a87e3f49a1a13859cf2b20ea2
Eric Debelak <ericdebelak@gmail.com>
2014-05-15 10:17:49 -0600

    moved log out

---
+++ logoutproc.php
@@ -0,0 +1,21 @@
+<?php
+	include("header.php");
+	// always use this to start/resume session
+	session_start();
+	
+	$_SESSION["id"] = null;
+	// first empty out the session array
+	$_SESSION = array();
+	
+	// second, delete the session cookie, if we're configured to use cookies (by default, php uses cookies)
+	if(ini_get("session.use_cookies")) // returns PHP config value from php.ini
+	{
+		$params = session_get_cookie_params();
+		setcookie(session_name(), "", 1, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+	}
+	// finally destroy the session
+	session_destroy();
+	echo "<section>You are now logged out.</section>";
+	include("footer.php");
+	
+?>
